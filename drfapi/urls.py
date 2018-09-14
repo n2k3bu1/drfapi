2b976ccb7cfb7e9ba5727582f732871761a90b8d
Ninh <bsninh@gmail.com>
2018-09-14 17:20:29 +0700

    new

---
+++ drfapi/urls.py
@@ -0,0 +1,42 @@
+"""drfapi URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.conf.urls import include, url
+from django.contrib import admin
+from rest_framework_nested import routers
+from authentication.views import IndexView, AccountViewSet, LoginView, LogoutView
+from posts.views import AccountPostsViewSet, PostViewSet
+
+admin.autodiscover()
+
+router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'accounts', AccountViewSet)
+
+router.register(r'posts', PostViewSet)
+accounts_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+accounts_router.register(r'posts', AccountPostsViewSet)
+
+urlpatterns = [
+    url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1/', include(accounts_router.urls)),
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
+    url(r'^$', IndexView.as_view(), name='index'),
+
+    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', admin.site.urls),
+]
---
+++ posts/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from authentication.models import Account
+
+
+class Post(models.Model):
+    author = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='posts')
+    content = models.TextField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{0}'.format(self.content)
